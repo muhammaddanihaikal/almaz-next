5594032c9da2c3fe1ca1bf203ce53e7aeddb637c
muhammaddanihaikal <dainnaxjakarta91@gmail.com>
2026-05-08 16:46:16 +0700

    style: increase layout max-width to 7xl

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -20,7 +20,7 @@ export default async function AppLayout({ children }) {
         />
         <div className="flex flex-1 flex-col overflow-hidden">
           <main className="flex-1 overflow-x-hidden overflow-y-auto">
-            <div className="mx-auto max-w-6xl px-4 py-6 lg:px-6 lg:py-8">
+            <div className="mx-auto w-full max-w-7xl px-4 py-6 lg:px-6 lg:py-8">
               <AppContent>
                 {children}
               </AppContent>
